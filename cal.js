4cb4b93e84b54941e5a16c214b1304f11c48dd29
YOGESHVAR P <yogeshvarp2004@gmail.com>
2025-08-11 14:47:09 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,26 @@
+while(true){
+var a=parseInt (prompt("Enter First Number"));
+var b=parseInt (prompt("Enter Second Number"));
+var op=prompt ("Enter Operator");
+var res;
+if(op==='+'){
+res=a+b;
+document.getElementById('output').innerHTML+='Output is '+res;
+break;
+}
+else if(op==='-'){
+res=a-b;
+document.getElementById('output').innerHTML+='Output is '+res; 
+break;
+}
+else if(op==='*'){
+    res=a*b;
+document.getElementById('output').innerHTML+='Output is '+res;
+break;
+}
+else if(op==='/'){
+    res=a/b;
+document.getElementById('output').innerHTML+='Output is '+res;
+break;
+}
+}
